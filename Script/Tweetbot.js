a4fce64b7e8c048248a3f69d602e4c82412ebfab
AWelook <88821171+AWelook@users.noreply.github.com>
2022-11-25 21:44:43 +0800

    Update Tweetbot.js

--- Script/Tweetbot.js
+++ Script/Tweetbot.js
@@ -1,6 +1,8 @@
 
+[rewrite_local]
 https://push.tapbots.com/tweetbot/5/verify_subscription url script-response-body https://github.com/AWelook/AWelook/raw/main/Script/Tweetbot.js
 
+[mitm]
 hostname=*.tapbots.com
 
 
